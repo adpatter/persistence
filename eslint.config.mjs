aedb1f25eeb47d9302b6e4b4d34f75496d3e9580
Adam Patterson <adpatter2000@gmail.com>
2026-02-22 03:20:51 +0000

    more

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import globals from "globals";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  { ignores: ["**/dist"] },
+  {
+    languageOptions: {
+      parserOptions: { project: ["./tsconfig.eslint.json"] },
+      globals: { ...globals.node },
+    },
+  },
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.js", "**/*.mjs", "**/*.cjs"],
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
+      "@stylistic/semi": ["error", "always"],
+      "@typescript-eslint/no-require-imports": ["off"],
+    },
+  },
+]);
